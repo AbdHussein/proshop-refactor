0ad50ef6ca263eca5ca58fce957eab938a1679c8
devmohammedna <ma7mmmed.n3im@gmail.com>
2022-02-01 18:09:06 +0200

    edit color typography

--- src/components/Typography/style.ts
+++ src/components/Typography/style.ts
@@ -20,10 +20,13 @@ const getTypographySize = (variant: string) => {
 };
 
 const Wrapper = styled('p')<any>`
-  /* color: ${props => props.color || 'black'}; */
-
   font-weight: ${props => (props?.bold || props?.variant === 'h2' ? 750 : 500)};
-  color: ${props => props.theme.text?.primary};
+  color: ${props =>
+    props.color === 'disabled'
+      ? props.theme.text.disabled
+      : props.color === 'secondary'
+      ? props.theme.text.secondary
+      : props.theme.text?.primary};
   font-size: ${props =>
     props?.fontSize || getTypographySize(props?.variant || '')};
   line-height: ${props => (props?.bold ? 1.1 : 1.25)};
